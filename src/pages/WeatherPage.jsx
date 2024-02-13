5fa2f8d416896585a848ff890a5bf76d2c646dc7
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2024-02-14 00:31:03 +0600

    weatherBoard Component created successfully

--- src/pages/WeatherPage.jsx
+++ src/pages/WeatherPage.jsx
@@ -2,6 +2,10 @@ import FavoriteListModal from '../components/Header/FavoriteListModal';
 import Favorites from '../components/Header/Favorites';
 import Header from '../components/Header/Header';
 import SearchLocation from '../components/Header/SearchLocation';
+import AddToFavorite from '../components/WeatherBoard/AddToFavorite';
+import WeatherBoard from '../components/WeatherBoard/WeatherBoard';
+import WeatherCondition from '../components/WeatherBoard/WeatherCondition';
+import WeatherHeadline from '../components/WeatherBoard/WeatherHeadline';
 
 export default function WeatherPages() {
     return (
@@ -11,6 +15,11 @@ export default function WeatherPages() {
                 <Favorites />
                 <FavoriteListModal />
             </Header>
+            <WeatherBoard>
+                <AddToFavorite />
+                <WeatherHeadline />
+                <WeatherCondition />
+            </WeatherBoard>
         </>
     );
 }
